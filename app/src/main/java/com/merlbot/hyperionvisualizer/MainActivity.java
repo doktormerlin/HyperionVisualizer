a422c8b7bc312a4b9c40a09f92995cb5d56a2c75
Merlin Dienst <merlin.dienst@gmail.com>
2018-04-01 14:59:22 +0200

    Initial Commit

---
+++ app/src/main/java/com/merlbot/hyperionvisualizer/MainActivity.java
@@ -0,0 +1,203 @@
+package com.merlbot.hyperionvisualizer;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+    private static final int FLAG_ACTIVITY_NEW_TASK = 1;
+    static boolean isActive = false;
+    static boolean hasPermission = false;
+    private static final int MY_PERMISSIONS_RECORD_AUDIO = 200;
+
+    static Intent visualizerIntent = null;
+    Button btn_start_stop_visualizer = null;
+    EditText edittext_ip_address = null;
+    EditText edittext_upd_port = null;
+    EditText edittext_led_count = null;
+    EditText edittext_visualizer_rate = null;
+    EditText edittext_color_rate = null;
+    EditText editText_color_starting_offset = null;
+    RadioButton radioButton_single_color = null;
+    RadioButton radioButton_rainbow = null;
+    RadioGroup radioGroup_visualizer_method = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final SharedPreferences prefs = MainActivity.this.getPreferences(Context.MODE_PRIVATE);
+        btn_start_stop_visualizer = findViewById(R.id.btn_start_stop_visualizer);
+        edittext_ip_address = findViewById(R.id.input_ip);
+        edittext_upd_port = findViewById(R.id.input_port);
+        edittext_led_count = findViewById(R.id.input_led_count);
+        edittext_visualizer_rate = findViewById(R.id.input_visualizer_rate);
+        edittext_color_rate = findViewById(R.id.input_color_rate);
+        editText_color_starting_offset = findViewById(R.id.input_color_starting_offset);
+        radioButton_single_color = findViewById(R.id.radiobutton_single_colour);
+        radioButton_rainbow = findViewById(R.id.radiobutton_rainbow);
+        radioGroup_visualizer_method = findViewById(R.id.radiogroup_visualizer_method);
+
+        visualizerIntent = new Intent(this, VisualizerService.class);
+
+        final Context context = MainActivity.this;
+
+        String prefs_ip_address = prefs.getString("ip_address", null);
+        int prefs_port = prefs.getInt("udp_port", -1);
+        int prefs_led_count = prefs.getInt("led_count", -1);
+        int prefs_visualizer_rate = prefs.getInt("visualizer_rate", -1);
+        float prefs_color_rate = prefs.getFloat("color_rate", -1);
+        int checked_button = prefs.getInt("checked_button", R.id.radiobutton_single_colour);
+
+        if(prefs_ip_address != null){
+            edittext_ip_address.setText(prefs_ip_address);
+        }
+        if(prefs_port != -1){
+            edittext_upd_port.setText(String.valueOf(prefs_port));
+        }
+        if(prefs_led_count != -1){
+            edittext_led_count.setText(String.valueOf(prefs_led_count));
+        }
+        if(prefs_visualizer_rate != -1){
+            edittext_visualizer_rate.setText(String.valueOf(prefs_visualizer_rate));
+        }
+        if(prefs_color_rate != -1){
+            edittext_color_rate.setText(String.valueOf(prefs_color_rate));
+        }
+
+        radioGroup_visualizer_method.check(checked_button);
+
+        requestAudioPermissions();
+
+
+        final TextView text_permission_info = findViewById(R.id.text_permission_info);
+        if(!hasPermission){
+            btn_start_stop_visualizer.setEnabled(false);
+            text_permission_info.setText(R.string.permission_info_no_permission);
+        }
+        btn_start_stop_visualizer.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String ip_address = edittext_ip_address.getText().toString();
+                int udp_port = Integer.parseInt(edittext_upd_port.getText().toString());
+                int visualizer_rate = Integer.parseInt(edittext_visualizer_rate.getText().toString());
+                int led_count = Integer.parseInt(edittext_led_count.getText().toString());
+                float color_rate = Float.parseFloat(edittext_color_rate.getText().toString());
+                int checked_button = radioGroup_visualizer_method.getCheckedRadioButtonId();
+                int color_offset = Integer.parseInt(editText_color_starting_offset.getText().toString());
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putString("ip_address", ip_address);
+                editor.putInt("udp_port", udp_port);
+                editor.putInt("led_count", led_count);
+                editor.putInt("visualizer_rate", visualizer_rate);
+                editor.putFloat("color_rate", color_rate);
+                editor.putInt("checked_button", checked_button);
+                editor.putInt("color_offset", color_offset);
+                editor.apply();
+
+                visualizerIntent.putExtra("ip_address", ip_address);
+                visualizerIntent.putExtra("port", udp_port);
+                visualizerIntent.putExtra("led_count", led_count);
+                visualizerIntent.putExtra("visualizer_rate", visualizer_rate);
+                visualizerIntent.putExtra("color_rate", color_rate);
+                visualizerIntent.putExtra("visualization_method", checked_button);
+                visualizerIntent.putExtra("color_offset", color_offset);
+
+                if(isActive){
+                    stopService(visualizerIntent);
+                    btn_start_stop_visualizer.setText(R.string.button_start);
+                }
+                else{
+                    startService(visualizerIntent);
+                    btn_start_stop_visualizer.setText(R.string.button_stop);
+                }
+
+                edittext_ip_address.setEnabled(isActive);
+                edittext_upd_port.setEnabled(isActive);
+                edittext_led_count.setEnabled(isActive);
+                edittext_visualizer_rate.setEnabled(isActive);
+                edittext_color_rate.setEnabled(isActive);
+                radioButton_rainbow.setEnabled(isActive);
+                radioButton_single_color.setEnabled(isActive);
+                editText_color_starting_offset.setEnabled(isActive);
+
+                isActive = !isActive;
+
+            }
+        });
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void requestAudioPermissions() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            //When permission is not granted by user, show them message why this permission is needed.
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                    Manifest.permission.RECORD_AUDIO)) {
+                Toast.makeText(this, "Please grant permissions to record audio", Toast.LENGTH_LONG).show();
+
+                //Give user option to still opt-in the permissions
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.RECORD_AUDIO},
+                        MY_PERMISSIONS_RECORD_AUDIO);
+
+            } else {
+                // Show user dialog to grant permission to record audio
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.RECORD_AUDIO},
+                        MY_PERMISSIONS_RECORD_AUDIO);
+            }
+        }
+        //If permission is granted, then go ahead recording audio
+        else if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.RECORD_AUDIO)
+                == PackageManager.PERMISSION_GRANTED) {
+            hasPermission = true;
+        }
+    }
+
+    //Handling callback
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_RECORD_AUDIO: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay!
+                    hasPermission = true;
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MainActivity.this, "Permissions Denied to record audio", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+        }
+    }
+
+}
+
---
+++ app/src/main/java/com/merlbot/hyperionvisualizer/VisualizerService.java
@@ -0,0 +1,209 @@
+package com.merlbot.hyperionvisualizer;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.AudioManager;
+import android.media.audiofx.Visualizer;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class VisualizerService extends Service {
+    static final String CHANNEL_DEFAULT_IMPORTANCE = "HIGH";
+    static final int SINGLE_COLOR = 0;
+    static final int RAINBOW_SWIRL = 1;
+    static String UDP_IP = "";
+    static int UDP_PORT = -1;
+    static int LED_COUNT = -1;
+    static int RATE = 0;
+    static int VISUALIZATION_METHOD = 0;
+    static float TURNAROUND_RATE = 0;
+    private static final int MY_PERMISSIONS_RECORD_AUDIO = 200;
+    private static final int ONGOING_NOTIFICATION_ID = 30;
+    static float turnaround = 0;
+    static Visualizer vis = null;
+    private Handler handler = new Handler();
+
+    public VisualizerService() {
+    }
+
+    private static final String TAG = "MyService";
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        Log.d(TAG, "onCreate");
+        super.onCreate();
+
+    }
+
+    @Override
+    public void onDestroy() {
+        Toast.makeText(this, "Stopped visualizing", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "onDestroy");
+        vis.setDataCaptureListener(null, 0, false, false);
+        vis.setEnabled(false);
+        handler.removeCallbacksAndMessages(null);
+        vis = null;
+        super.onDestroy();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startid) {
+        Log.d(TAG, "onStartCommand");
+        UDP_IP = intent.getStringExtra("ip_address");
+        UDP_PORT = intent.getIntExtra("port", UDP_PORT);
+        LED_COUNT = intent.getIntExtra("led_count", LED_COUNT);
+        RATE = intent.getIntExtra("visualizer_rate", RATE);
+        TURNAROUND_RATE = intent.getFloatExtra("color_rate", TURNAROUND_RATE);
+        VISUALIZATION_METHOD = intent.getIntExtra("visualization_method", VISUALIZATION_METHOD);
+        turnaround = intent.getIntExtra("color_offset", (int) turnaround);
+
+
+        Toast.makeText(this, "Visualizing to IP " + UDP_IP, Toast.LENGTH_LONG).show();
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        Notification notification =
+                new Notification.Builder(this, "TEST")
+                        .setContentTitle(getText(R.string.foreground_service_notification_title))
+                        .setContentText(getText(R.string.foreground_service_notification_title))
+                        .setSmallIcon(R.drawable.icon)
+                        .setContentIntent(pendingIntent)
+                        .setTicker(getText(R.string.ticker_text))
+                        .build();
+
+        startForeground(ONGOING_NOTIFICATION_ID, notification);
+        vis = new Visualizer(0);
+        MyVisualizer visListener = new MyVisualizer();
+        vis.setDataCaptureListener(visListener, RATE, true, false);
+        int res = vis.setEnabled(true);
+        return super.onStartCommand(intent, flags, startid);
+    }
+
+    class MyVisualizer implements Visualizer.OnDataCaptureListener{
+        final String TAG = "MyVisualizer";
+
+        private void sendBytes(byte[] bytes){
+            AudioManager manager = (AudioManager)VisualizerService.this.getSystemService(Context.AUDIO_SERVICE);
+            int steps = bytes.length / LED_COUNT;
+            float[] hues = new float[96];
+            for(int j = 0; j < LED_COUNT; j++){
+                int tmp_avg = 0;
+                for(int k = 0; k < steps; k++){
+                    tmp_avg += (int) bytes[steps*j+k] & 0xFF;
+                }
+                float intres = tmp_avg / (float) steps;
+                hues[j] = intres/(float) 255;
+            }
+
+            MyVisualizer.MyInnerThread mit = new MyVisualizer.MyInnerThread(hues);
+            mit.start();
+
+        }
+
+        @Override
+        public void onWaveFormDataCapture(Visualizer visualizer, byte[] bytes, int i) {
+            sendBytes(bytes);
+        }
+
+
+        @Override
+        public void onFftDataCapture(Visualizer visualizer, byte[] bytes, int i) {
+            sendBytes(bytes);
+        }
+
+        class MyInnerThread extends Thread{
+            float[] hues;
+            MyInnerThread(float[] hues){
+                this.hues = hues;
+            }
+
+            public void run(){
+                sendByteArray(getByteArray(hues));
+            }
+
+            byte[] getByteArray(float[] vs){
+                byte[] bytes = new byte[LED_COUNT*3];
+                int[] hues = new int[LED_COUNT];
+
+                if(VISUALIZATION_METHOD == R.id.radiobutton_rainbow){
+                    hues = rainBowSwirl();
+                }else if(VISUALIZATION_METHOD == R.id.radiobutton_single_colour){
+                    hues = single_color();
+                }else{
+                    for(int i = 0; i < LED_COUNT; i++){
+                        hues[i] = 0;
+                    }
+                }
+
+                for(int i = 0; i < LED_COUNT; i++){
+                    float[] hsv = new float[]{(float) hues[i], 1, vs[i]};
+                    int color = Color.HSVToColor(hsv);
+                    bytes[i*3] = (byte) Color.red(color);
+                    bytes[i*3+1] = (byte) Color.green(color);
+                    bytes[i*3+2] = (byte) Color.blue(color);
+                }
+                return bytes;
+            }
+
+            int[] rainBowSwirl(){
+                int[] res = new int[LED_COUNT];
+                for(int i = 0; i < LED_COUNT; i++){
+                    res[i] = ((int) (((float) i / LED_COUNT) * 360) + (int) turnaround) % 360;
+                }
+                turnaround += TURNAROUND_RATE;
+                return res;
+            }
+
+            int[] single_color(){
+                int[] res = new int[LED_COUNT];
+                for(int i = 0; i < LED_COUNT; i++){
+                    res[i] = (int) turnaround % 360;
+                }
+                turnaround += TURNAROUND_RATE;
+                return res;
+            }
+
+            void sendByteArray(byte[] bytes){
+                DatagramSocket ds = null;
+                try {
+                    ds = new DatagramSocket();
+                    InetAddress serverAddr = InetAddress.getByName(UDP_IP);
+                    DatagramPacket dp;
+                    dp = new DatagramPacket(bytes, bytes.length, serverAddr, UDP_PORT);
+                    ds.send(dp);
+                } catch (SocketException e) {
+                    e.printStackTrace();
+                }catch (UnknownHostException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (ds != null) {
+                        ds.close();
+                    }
+                }
+            }
+        }
+    }
+}
